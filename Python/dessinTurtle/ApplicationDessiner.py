3521f63e0372f511ee366b51aceddbc280b8696b
woittequand <woittequandyohan@yahoo.fr>
2016-04-24 15:38:04 +0200

    Démarrage structure en mode console

---
+++ Python/dessinTurtle/ApplicationDessiner.py
@@ -0,0 +1,15 @@
+#/usr/bin/python3.5
+# coding:utf-8
+
+print("{0}{1}Dessiner c'est gagner{1}{0}".format("#"*8,"-"*3))
+
+print()
+
+commencePartie= input("Voulez-vous démarrer un partie ? [ O - N ]")
+#Faire un fonction de vérification en fonction des erreurs possible pour renvoyer un boolean
+
+print(commencePartie)
+
+
+
+print("{0}{1}Fin du jeu ... A bientôt {1}{0}".format("#"*8,"-"*3))
